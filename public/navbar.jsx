170e767fc2d1f2f90967ecdfecbf25a8a84ce655
Crescens <2e3crescens@gmail.com>
2024-02-29 17:54:27 +0800

    Testing login and logout func

--- public/navbar.jsx
+++ public/navbar.jsx
@@ -1,67 +1,110 @@
 function NavBar() {
+  const { authUser, setAuthUser, isLoggedIn, setIsLoggedIn } = useAuth();
+
+  const logIn = (e) => {
+    e.preventDefault();
+    setIsLoggedIn(true);
+    setAuthUser({
+      Name: "John Doe",
+    });
+  };
+  const logOut = (e) => {
+    e.preventDefault();
+    setIsLoggedIn(false);
+    setAuthUser(null);
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          {" "}
-          BadBank{" "}
-        </a>
+    <>
+      {" "}
+      <span>User is currently: {isLoggedIn ? "Logged-In" : "Logged Out"}.</span>
+      {isLoggedIn ? <span>User name: {authUser.Name}</span> : null}
+      <br />
+      {isLoggedIn ? (
         <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          onClick={(e) => {
+            logOut(e);
+          }}
         >
-          <span className="navbar-toggler-icon"></span>
+          Log Out
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a
-                className="nav-link active"
-                aria-current="page"
-                href="#/createAccount"
-              >
-                Create Account
+      ) : (
+        <button
+          onClick={(e) => {
+            logIn(e);
+          }}
+        >
+          Log In
+        </button>
+      )}
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            {" "}
+            BadBank{" "}
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="#/createAccount"
+                >
+                  Create Account
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#/login/">
+                  {" "}
+                  Login{" "}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#/deposit/">
+                  {" "}
+                  Deposit{" "}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#/withdraw/">
+                  {" "}
+                  Withdraw{" "}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#/balance/">
+                  {" "}
+                  Balance{" "}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#/allData/">
+                  {" "}
+                  AllData{" "}
+                </a>
+              </li>
+            </ul>
+            <span className="navbar-text">
+              <a className="nav-link" href="#">
+                Logout
               </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#/login/">
-                {" "}
-                Login{" "}
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#/deposit/">
-                {" "}
-                Deposit{" "}
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#/withdraw/">
-                {" "}
-                Withdraw{" "}
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#/balance/">
-                {" "}
-                Balance{" "}
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#/allData/">
-                {" "}
-                AllData{" "}
-              </a>
-            </li>
-          </ul>
+            </span>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
 
